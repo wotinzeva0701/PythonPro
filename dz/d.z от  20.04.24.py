de9befd52df1cdb01d7609f162af2cedab4d5839
Julia <wojw0701@gmail.com>
2024-04-23 12:42:06 +0300

    dom.z

--- dz/d.z от  20.04.24.py
+++ dz/d.z от  20.04.24.py
@@ -1,31 +1,3 @@
-# class Fiqure:
-#     def __init__(self, color):
-#         self.__color = color
-#
-#     @property
-#     def color(self):
-#         return self.__color
-#
-#     @color.setter
-#     def color(self, c):
-#         self.__color = c
-#
-#
-# class Rectangle(Fiqure):
-#     def __init__(self, width, height, color):
-#         super().__init__(color)
-#         self.__width = width
-#         self.__height = height
-#
-#     def area(self):
-#         print(f"Прямоугольник {self.color}. Площадь: ", end="")
-#         return self.__width * self.__height
-#
-#
-# rect = Rectangle(10, 20, "green")
-# print(rect.area())
-
-
 class Figure:
     def __init__(self, color):
         self.__color = color
